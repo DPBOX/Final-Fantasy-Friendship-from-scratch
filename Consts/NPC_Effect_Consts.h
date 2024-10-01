4fb773b581243903be90381afbdd2d287920a4f9
DPBOX <88913370+DPBOX@users.noreply.github.com>
2024-09-30 19:58:53 -0500

    Delete Consts/NPC_Effect_Consts.h

--- Consts/NPC_Effect_Consts.h
+++
@@ -1,7 +0,0 @@
-#ifndef OBJ_CONSTS_H
-#define OBJ_CONSTS_H
-
-const NPC_Effect NONE_EFFECT{NPC_Effect(32, 12, -28, binary_Image_simple_panel_png_start, reinterpret_cast<long>(&binary_Image_simple_panel_png_size), 90, vector<long>{0, 1, 2, 3})};
-//const NPC_Effect SLEEP_EFFECT{NPC_Effect(32, 12, -28, binary_Image_sleeping_effect_png_start, reinterpret_cast<long>(&binary_Image_sleeping_effect_png_size), 90, vector<long>{0, 1, 2, 3})};
-
-#endif
