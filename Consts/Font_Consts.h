7d7fb3d0764a2c493633dbbc0eb1284765cdbabe
DPBOX <88913370+DPBOX@users.noreply.github.com>
2024-09-30 19:59:58 -0500

    Delete Consts/Font_Consts.h

--- Consts/Font_Consts.h
+++
@@ -1,8 +0,0 @@
-#ifndef FONT_CONSTS_H
-#define FONT_CONSTS_H
-
-const Font_Params TEXT_FONT{};
-const Font_Params HEADING_FONT{"Heading", binary_Image_heading_font_png_start, reinterpret_cast<long>(&binary_Image_heading_font_png_size), FONT_HEADING_CELL_WIDTH, FONT_HEADING_CHAR_WIDTHS};
-const Font_Params CHARACTER_TITLE_FONT{"Character Title", binary_Image_text_font_yellow_png_start, reinterpret_cast<long>(&binary_Image_text_font_yellow_png_size), FONT_TEXT_CELL_WIDTH, FONT_TEXT_CHAR_WIDTHS};
-
-#endif
